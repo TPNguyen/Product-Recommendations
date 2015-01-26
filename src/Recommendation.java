05db12b58f0faf2183462559c848d2f2e23cac81
Cleophus Robinson IV <CleoR41@gmail.com>
2015-01-26 08:49:40 -0800

    Finished counting all of the items

--- src/Recommendation.java
+++ src/Recommendation.java
@@ -15,9 +15,22 @@ public class Recommendation {
 		outputFile = args[1];
 		//Set the class variable
 		singleCounts = new HashMap<String,Integer>();
+		pairCounts = new HashMap<HashSet<String>,Integer>();
+		tripleCounts = new HashMap<HashSet<String>,Integer>();
 		threshold = 100;
 		firstPass();
+		System.out.println("1st Candidates: " + singleCounts.size());
 		pruneSingles();
+		System.out.println("1st Official: " + singleCounts.size());
+		secondPass();
+		System.out.println("2nd Candidates: " + pairCounts.size());
+		prunePairs();
+		System.out.println("2nd Official: " + pairCounts.size());
+		thirdPass();
+		System.out.println("3rd Candidates: " + tripleCounts.size());
+		pruneTriples();
+		System.out.println("3rd Official: " + tripleCounts.size());
+		System.out.println("Finished");
 	}
 	
 	//All used in the first step of the algorithm
@@ -33,7 +46,6 @@ public class Recommendation {
 			}
 		}
 	}
-
 	private static void pruneSingles(){
 		Iterator<Map.Entry<String,Integer>> iter = singleCounts.entrySet().iterator();
 		while (iter.hasNext()) {
@@ -43,7 +55,6 @@ public class Recommendation {
 		    }
 		}
 	}
-
 	private static void firstPass() throws IOException{
 		// Construct BufferedReader from FileReader
 		File fi = new File(inputFile);
@@ -55,31 +66,111 @@ public class Recommendation {
 		br.close();
 	}
 	//All used in the first step of the algorithm
-	
-	
-	private static void makePairs(String value){
+	//All used in the second step of the algorithm
+	private static HashSet<HashSet<String>> makePairs(String value){
 	   String[] ar = value.split("\\s+");
+	   HashSet<HashSet<String>> basketPairs = new HashSet<HashSet<String>>();
 	   int size = ar.length;
-	   if(size < 2) return;
 	   for(int i = 0; i < size; i++){
 		   String item1 = ar[i];
 		   for(int j = 0; j < size; j++){
-			   if(i != j){
+			   if(i!=j){
 				   String item2 = ar[j];
 				   //Both items are frequent
 				   if(singleCounts.containsKey(item1) && singleCounts.containsKey(item2)){
 					   HashSet<String> pair = new HashSet<String>(2);
-					   if(pairCounts.containsKey(pair)){
-			            	pairCounts.put(pair, pairCounts.get(pair)+1);
-			            }else{
-			            	pairCounts.put(pair,1);
-			            }
+					   pair.add(item1);
+					   pair.add(item2);
+					   basketPairs.add(pair);
 				   }
 			   }
 		   }
 	   }
-   }
+	   return basketPairs;
+	}
+	private static void countPairs(String value){
+		HashSet<HashSet<String>> basketPairs = makePairs(value);
+		for(HashSet<String> pair: basketPairs){
+			if(pairCounts.containsKey(pair)){
+				pairCounts.put(pair, pairCounts.get(pair)+1);
+			}else{
+				pairCounts.put(pair,1);
+			}
+		}
+   	}
+	private static void prunePairs(){
+		Iterator<Map.Entry<HashSet<String>,Integer>> iter = pairCounts.entrySet().iterator();
+		while (iter.hasNext()) {
+		    Map.Entry<HashSet<String>,Integer> entry = iter.next();
+		    if(entry.getValue() < threshold){
+		        iter.remove();
+		    }
+		}
+	}
+	private static void secondPass() throws IOException{
+		// Construct BufferedReader from FileReader
+		File fi = new File(inputFile);
+		BufferedReader br = new BufferedReader(new FileReader(fi));
+		String line = null;
+		while ((line = br.readLine()) != null) {
+			countPairs(line);
+		}
+		br.close();
+	}
+	//All used in the second step of the algorithm
+	//All used in the third step of the algorithm
+	private static HashSet<HashSet<String>> makeTriples(String value){
+		HashSet<HashSet<String>> basketTriples = new HashSet<HashSet<String>>();
+		HashSet<HashSet<String>> basketPairs = makePairs(value);
+		for(HashSet<String> pair1: basketPairs){
+			for(HashSet<String> pair2: basketPairs){
+				if(!pair2.equals(pair1) && pairCounts.containsKey(pair1) && pairCounts.containsKey(pair2)){
+					HashSet<String> triple = new HashSet<String>(4);
+					triple.addAll(pair1);
+					triple.addAll(pair2);
+					//A very crude way of making sure the pairs have at least 1 element in common
+					if(triple.size() == 3){
+						basketTriples.add(triple);
+					}
+				}
+			}
+		}
+		return basketTriples;
+	}
+	private static void countTriples(String value){
+		HashSet<HashSet<String>> basketTriples = makeTriples(value);
+		for(HashSet<String> triple: basketTriples){
+			if(tripleCounts.containsKey(triple)){
+				tripleCounts.put(triple, tripleCounts.get(triple)+1);
+			}else{
+				tripleCounts.put(triple,1);
+			}
+		}
+   	}
+	private static void pruneTriples(){
+		Iterator<Map.Entry<HashSet<String>,Integer>> iter = tripleCounts.entrySet().iterator();
+		while (iter.hasNext()) {
+		    Map.Entry<HashSet<String>,Integer> entry = iter.next();
+		    if(entry.getValue() < threshold){
+		        iter.remove();
+		    }
+		}
+	}
+	private static void thirdPass() throws IOException{
+		// Construct BufferedReader from FileReader
+		File fi = new File(inputFile);
+		BufferedReader br = new BufferedReader(new FileReader(fi));
+		String line = null;
+		while ((line = br.readLine()) != null) {
+			countTriples(line);
+		}
+		br.close();
+	}
+	//All used in the third step of the algorithm
 	
+	
+	
+	//This is just the model for reading the file
 	private static void readFile() throws IOException{
 		// Construct BufferedReader from FileReader
 		File fi = new File(inputFile);
